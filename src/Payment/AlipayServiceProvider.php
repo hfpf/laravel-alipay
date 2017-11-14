45d149930dfe015f9b7979cd0282e4b9783f5674
huangfu <hfpf@live.cn>
2017-11-14 16:04:50 +0800

    initial commmit

---
+++ src/Payment/AlipayServiceProvider.php
@@ -0,0 +1,56 @@
+<?php namespace Huangfu\LaravelAlipay;
+
+use Alipay\Payment\Payment;
+use Illuminate\Support\ServiceProvider;
+
+class AlipayServiceProvider extends ServiceProvider
+{
+
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupConfig();
+    }
+
+    public function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/alipay-payment.php');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                $source => config_path('alipay-payment.php')
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Payment::class, function ($app) {
+
+            $payment = new Payment(config('alipay-payment'));
+            return $payment;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            Payment::class
+        ];
+    }
+}
---
+++ src/Payment/alipay-payment.php
@@ -0,0 +1,32 @@
+<?php
+$config = [
+    // 应用ID,您的APPID(必填)
+    'app_id' => 'xxxxxxxxxxxx',
+
+    // 私钥文件路径（秘钥和秘钥文件路径不能同时为空）(
+    //'private_key_file_path' => null,
+
+    // 公钥文件路径（秘钥和秘钥文件路径不能同时为空）
+    //'public_key_file_path' => null,
+
+    // 商户私钥（秘钥和秘钥文件路径不能同时为空）
+    'private_key' => 'your-private-key',
+
+    // 支付宝公钥（秘钥和秘钥文件路径不能同时为空）,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    'public_key' => 'your-public-key',
+
+    // 默认异步通知地址（可选配置项:默认为""）
+    //'notify_url' => 'http://api.test.alipay.net/atinterface/receive_notify.htm',
+
+    // 默认同步跳转地址（可选配置项:默认为""）
+    //'return_url' => '',
+
+    // 编码格式（可选配置项,默认为UTF-8）
+    //'charset' => 'UTF-8',
+
+    // 签名方式（可选配置项,默认为RSA2）
+    //'sign_type'=>'RSA2',
+
+    // 支付宝网关（可选配置项,默认为https://openapi.alipay.com/gateway.do）
+    //'gatewayUrl' => ''
+];
